4afb9e8e2ee5ee4d24bb1ddf8a272db2c8a7b267
EvansMaingi <Evans.Maingi@strathmore.edu>
2025-05-10 14:50:43 +0300

    The Teachers Module

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use App\Models\Teacher;
+use Illuminate\View\View;
+class TeacherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index():View 
+    {
+
+    
+        $teachers = Teacher::all();
+        return view ('teacher.index')->with('teachers', $teachers);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create():View
+    {
+        return view('teacher.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $input = $request->all();
+        Teacher::create($input);
+        return redirect('teachers')->with('flash_message', 'teacher Addedd!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id):View
+    {
+        
+        $teachers = Teacher::find($id);
+        return view('teacher.show')->with('teachers', $teachers);
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id):View
+    {
+        $teachers= Teacher::find($id);
+        return view('teacher.edit')->with('teachers', $teachers);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id):RedirectResponse
+    {
+        $teachers = Teacher::find($id);
+        $input = $request->all();
+        $teachers->update($input);
+        return redirect('teachers')->with('flash_message', 'teacher Updated!');  
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id):RedirectResponse
+    {
+        Teacher::destroy($id);
+        return redirect('teachers')->with('flash_message', 'teacher deleted!');  
+    }
+}
